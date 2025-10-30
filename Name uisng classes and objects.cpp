600634dbe3af51999da37dd9de06afbc217146ef
ZainabWasim-040 <zainabwasim902@gmail.com>
2025-10-30 11:40:06 +0500

    Add files via upload

---
+++ Name uisng classes and objects.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class Name
+{
+	public:
+		void display()
+		{
+			cout<<"Zainab Wasim";
+		}
+};
+int main()
+{
+	Name obj;
+	obj.display();
+	getch();
+}
+
