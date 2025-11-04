73183e4442e94d0a25278e38868b994327b217ae
ZainabWasim-040 <zainabwasim902@gmail.com>
2025-11-04 22:14:56 +0500

    Add files via upload

---
+++ LAB 05  Q1.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class Count
+{
+   private:
+   int n;
+   public:
+   Count() //invoke
+   {
+   	 n=0;
+   }
+   void show()
+   {
+   	cout<<"Value of n is: "<<n<<endl;
+	}
+	void operator++() //prefix operator overloading
+	{
+		n=n+1;
+	}
+	void operator++(int) //postfix operator overloading
+	{
+		n=n+1;   
+	}	
+};
+int main()
+{
+	Count obj;
+	obj.show();
+	++obj;
+	obj.show();
+	obj++;
+	obj.show();
+}
